b4c56e702dcb38966571a671a574821ad365a7d1
gui-motas <monjogui@gmail.com>
2024-11-10 14:01:30 -0300

    Terceira Questão

---
+++ questao-03/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k < indice) {
+            k += 1;
+            soma += k;
+        }
+
+        System.out.println(soma);
+
+    }
+}
